aeba12d1aa24ab5182cc26e46a19269dc91795e3
YounesChefou <35134735+YounesChefou@users.noreply.github.com>
2019-10-08 21:01:24 +0200

    Update main.cpp

--- Software/main.cpp
+++ Software/main.cpp
@@ -1,25 +1,29 @@
 #include "mbed.h"
 #include "MMA8451.h"
 #include "st059.h"
+#include "bender.h"
 
 Timer t10sec;
 I2C i2c(D12, A6); //I2C3 
 MMA8451 accelerometre(i2c, 0x1D);
 Serial sigfox(D1, D0);
-//DigitalOut alimWisol(D2);
-//DigitalOut alimMMA(D4);
+DigitalOut alimentation(D2); // pin de controle de l'alimentation : D2 = 1 <=> passant  
+
 int xAccel, yAccel, zAccel;
 
 int n = 0; //nombre d'itérations d'envoi de données
+
 int main() {
+   
+   alimentation = 0;
     
     while(1){
             
             //accelerometre.setStandbyMode(); //l'accelerometre est en veille lorsqu'il n'y a pas de vibrations.
             //Mettre le module Wisol en veille
             //sigfox.printf("AT$P=1")
-        
-            if(detection_st059() == 1){
+            
+            if(detection_st059() == 1 && mesure_seuil() !=-1){
                 printf("Vibration\r\n");
                 t10sec.start(); 
                 accelerometre.setActiveMode();

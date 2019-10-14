080adab65e3fde1ac13ee8ff5dea88f6f7703cbd
YounesChefou <35134735+YounesChefou@users.noreply.github.com>
2019-10-14 11:57:38 +0200

    Update st059.cpp

--- Software/ST059/st059.cpp
+++ Software/ST059/st059.cpp
@@ -5,17 +5,11 @@
 #include "st059.h"
 
 #endif
-//DigitalOut led3(LED1);  // LD3 sur la carte
-//DigitalIn S(D13);     //entrée S du capteur
 
-DigitalIn ST059(D3);        //entrée S du capteur
+DigitalOut led3(LED1);  // LD3 sur la carte
+DigitalIn S(D13);     //entrée S du capteur
 
 int detection_st059(){ //detecte les vibrations et allume la led ld3.
-
-    return ST059.read();
-}
-
-/*int detection_st059(){ //detecte les vibrations et allume la led ld3.
 
     int val = S.read();
      
@@ -24,4 +18,4 @@ int detection_st059(){ //detecte les vibrations et allume la led ld3.
     else led3.write(0);
     
     return val;
-}*/
+}
